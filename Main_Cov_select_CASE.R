e270b33348ca4596da4ad4b91065e4d77be8e2fd
徐徐徐徐徐 <orangehsu.1998@gmail.com>
2022-03-31 20:52:34 +0800

    update

--- Main_Cov_select_CASE.R
+++ Main_Cov_select_CASE.R
@@ -11,20 +11,20 @@ CC.1 <- BiMMforest1(traindata = CCTr, testdata = CCTe,
                       seed = seed, glmControl = "maxfun")
 CC.1$`model summary`  
 CC.1$`CM of Train data`
-CC.1$`Train acc sen spe`
+CC.1$`Train acc sen spe`#1 1 1
 CC.1$`CM of Test data`
-CC.1$`Test acc sen spe`
+CC.1$`Test acc sen spe`#0.80952381 1.00000000 0.04761905
 CC.1$`lme.CM of Test data`
-CC.1$`lme.Test acc sen spe`
+CC.1$`lme.Test acc sen spe`#0.6809524 0.7500000 0.4047619
 
 
 
 CC.rf <-RF(traindata = CCTr, testdata = CCTe,
                   formula = F1, classwt = NULL,seed = seed)
 CC.rf$`CM of Train data`
-CC.rf$`Train acc sen spe`
+CC.rf$`Train acc sen spe`#0.9057279 1.0000000 0.5863874
 CC.rf$`CM of Test data`
-CC.rf$`Test acc sen spe`
+CC.rf$`Test acc sen spe`#0.9095238 1.0000000 0.6274510
 CC.rf$`Var importance`
 
 
@@ -35,11 +35,11 @@ CN.1 <- BiMMforest1(traindata = CNTr, testdata = CNTe,
                     seed = seed, glmControl = "maxfun")
 CN.1$`model summary`
 CN.1$`CM of Train data`
-CN.1$`Train acc sen spe`
+CN.1$`Train acc sen spe`# 1 1 1
 CN.1$`CM of Test data`
-CN.1$`Test acc sen spe`
+CN.1$`Test acc sen spe`#0.71621622 0.92352941 0.03846154
 CN.1$`lme.CM of Test data`
-CN.1$`lme.Test acc sen spe`
+CN.1$`lme.Test acc sen spe`# 0.765 1 0
 
 
 
