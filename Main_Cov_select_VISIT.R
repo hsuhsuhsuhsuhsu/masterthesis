1752c63b4b60b65254e221f6a678b52d9c51a43c
徐徐徐徐徐 <orangehsu.1998@gmail.com>
2022-04-04 23:55:46 +0800

    update

---
+++ Main_Cov_select_VISIT.R
@@ -0,0 +1,9 @@
+source("Train_Test_file_cov_select.r")
+source("DataProcFunctions.r")
+source("ModelBuildFunction.r")
+#
+F5 <- dip~visit+sbp+dbp+Gender+Age+HR+Drug_conut+DM+time+HOS+BMI+Waist+Walk_TM_week+anti_HP+office_peri_L_sys+office_peri_L_dia
+F6 <- dip~visit+sbp+dbp+time+HOS
+F7 <- factor(dip)~visit+sbp+dbp+Gender+Age+HR+Drug_conut+DM+time+HOS+BMI+Waist+Walk_TM_week+anti_HP+office_peri_L_sys+office_peri_L_dia
+F8 <- factor(dip)~visit+sbp+dbp+time+HOS
+seed = 123
